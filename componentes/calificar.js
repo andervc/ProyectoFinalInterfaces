fcc8d04c1d3feee9b32e3e5d53a6a5a0f5ef0b68
Anderson Valverde Cordero <avalverde@itcr.ac.cr>
2021-04-21 22:15:07 -0600

    Second Commit

--- componentes/calificar.js
+++ componentes/calificar.js
@@ -1,20 +1,20 @@
 
 
-function guardar(idPelicula, tituloPelicula, imagenPelicula, descripcionPelicula){
-    var existia=false
-    if(calificacion(idPelicula)==0){
+function guardar(idPelicula, tituloPelicula, imagenPelicula, descripcionPelicula) {
+    var existia = false
+    if (calificacion(idPelicula) == 0) {
         alert("Para guardar la Película es necesario asignar una Calificación")
     }
-    else{
-        if(!localStorage.getItem("PELICULAS")){
+    else {
+        if (!localStorage.getItem("PELICULAS")) {
             var peliculas = [
             ]
             localStorage.setItem("PELICULAS", JSON.stringify(peliculas))
         }
-       
-        if(buscar(idPelicula)>=0){
+
+        if (buscar(idPelicula) >= 0) {
             existia = true
-            eliminar(idPelicula,0)
+            eliminar(idPelicula, 0)
         }
         let nueva = {
             id: idPelicula,
@@ -23,83 +23,73 @@ function guardar(idPelicula, tituloPelicula, imagenPelicula, descripcionPelicula
             descripcion: descripcionPelicula,
             valoracion: calificacion(idPelicula)
         }
-        let guardadas= obtener()
-        guardadas.push(nueva)           
-        
-        
+        let guardadas = obtener()
+        guardadas.push(nueva)
+
+
         localStorage.setItem("PELICULAS", JSON.stringify(guardadas))
-        if(existia == false){
+        if (existia == false) {
             alert(`La película ${tituloPelicula} ha sido agregada a Favoritas`)
-        }else{
+        } else {
             alert(`La calificación de ${tituloPelicula} ha sido actualizada en Favoritas`)
         }
-        
-        // document.getElementById("exampleModal"+idPelicula).
-        // location.reload();
+
     }
 }
 
-function calificacion(id){
+function calificacion(id) {
     let puntuacion = 0
-    if(document.getElementById('radio1-'+id).checked){
-        puntuacion=5
-    }else{
-        if(document.getElementById('radio2-'+id).checked){
-            puntuacion=4
-        }else{
-            if(document.getElementById('radio3-'+id).checked){
-                puntuacion=3
-            }else{
-                if(document.getElementById('radio4-'+id).checked){
-                    puntuacion=2
-                }else{
-                    if(document.getElementById('radio5-'+id).checked){
-                        puntuacion=1
-                    }else{
-                        puntuacion=0
-                    }
-                }
-            }
-        }
+    if (document.getElementById('radio1-' + id).checked) {
+        puntuacion = 5
+    } else if (document.getElementById('radio2-' + id).checked) {
+        puntuacion = 4
+    } else if (document.getElementById('radio3-' + id).checked) {
+        puntuacion = 3
+    } else if (document.getElementById('radio4-' + id).checked) {
+        puntuacion = 2
+    } else if (document.getElementById('radio5-' + id).checked) {
+        puntuacion = 1
+    } else {
+        puntuacion = 0
     }
     return puntuacion
 }
 
-function obtener(){
-    if(localStorage.getItem("PELICULAS")){
+function obtener() {
+    if (localStorage.getItem("PELICULAS")) {
 
         let peliculas = JSON.parse(localStorage.getItem("PELICULAS"))
 
         //console.log(peliculas)
         return peliculas
 
-    }else{
-        
+    } else {
+
         console.log("No existe en el local storage")
     }
-    
+
 }
 
-function eliminar(id, tipo){
+function eliminar(id, tipo) {
     let favoritas = JSON.parse(localStorage.getItem("PELICULAS"))
-    let index = favoritas.findIndex(favoritas => favoritas.id==id)
+    let index = favoritas.findIndex(favoritas => favoritas.id == id)
 
     favoritas.splice(index, 1)
-   
+
     localStorage.setItem("PELICULAS", JSON.stringify(favoritas))
-    if(tipo==1){
+    if (tipo == 1) {
         alert(`La película ha sido eliminada de Favoritas`)
         location.reload()
     }
 }
 
-function buscar(id){
-    if(localStorage.getItem("PELICULAS")){
-    let favoritas = JSON.parse(localStorage.getItem("PELICULAS"))
-    var index = favoritas.findIndex(favoritas => favoritas.id==id)      
-    //console.log("Buscar = "+index)
-    }else{
-        index=-1
+function buscar(id) {
+    if (localStorage.getItem("PELICULAS")) {
+        let favoritas = JSON.parse(localStorage.getItem("PELICULAS"))
+        var index = favoritas.findIndex(favoritas => favoritas.id == id)
+        //console.log("Buscar = "+index)
+    } else {
+        index = -1
     }
     return index
 }
--- componentes/lazyLoading.js
+++ componentes/lazyLoading.js
@@ -2,23 +2,6 @@ const lazyLoading = ()=>{
 
 const images = document.querySelectorAll('img');
 
-// const lazyImage = (entries, observer) => {
-//     entries
-//     .filter(entry => entry.isIntersecting)
-//     .forEach((entry) => {
-//         const img = entry.target;
-//         const src = img.getAttribute('data-lazy');
-
-//         img.setAttribute('src', src);
-//         observer.disconnect();
-//         console.log('load');
-//     })
-// }
-
-// images.forEach((img) _> {
-//     const observer = new IntersectionObserver(lazyImage);
-//     observer.observe(img);
-// });
 const options ={
     threshold: .5
 }
